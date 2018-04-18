73bea691cd1498e2aa201de983411303382ad363
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-18 16:44:10 +0100

    Generate a new Gem

---
+++ lib/infreemation/version.rb
@@ -0,0 +1,3 @@
+module Infreemation
+  VERSION = "0.1.0"
+end
---
+++ spec/infreemation_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Infreemation do
+  it "has a version number" do
+    expect(Infreemation::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
