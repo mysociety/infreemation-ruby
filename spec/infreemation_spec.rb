dd103cc8c914f97fd60b2d0d107a3a8363c238ad
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-18 16:58:23 +0100

    Auto correct Rubocop warnings

--- spec/infreemation_spec.rb
+++ spec/infreemation_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe Infreemation do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Infreemation::VERSION).not_to be nil
   end
 end
