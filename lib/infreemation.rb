84d3d823654edf752d0784c16bf8f3a49b5da033
Graeme Porteous <graeme@rgbp.co.uk>
2018-04-20 09:27:24 +0100

    Add methods to assign configuration
    We will need access to the server URL, API key and username.

--- lib/infreemation.rb
+++ lib/infreemation.rb
@@ -6,5 +6,27 @@ require 'infreemation/version'
 # This module is the main entry point of the Gem
 #
 module Infreemation
-  # Your code goes here...
+  ConfigurationError = Class.new(StandardError)
+
+  class << self
+    attr_writer :url, :api_key, :username
+
+    def url
+      @url || raise(
+        ConfigurationError, 'Infreemation.url not configured'
+      )
+    end
+
+    def api_key
+      @api_key || raise(
+        ConfigurationError, 'Infreemation.api_key not configured'
+      )
+    end
+
+    def username
+      @username || raise(
+        ConfigurationError, 'Infreemation.username not configured'
+      )
+    end
+  end
 end
